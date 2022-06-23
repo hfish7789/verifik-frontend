12347a80f3c766426e96f42fc28182c150bb4d25
hfish7789 <hfish7789@gmail.com>
2022-06-23 18:00:56 +0900

    final commit

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,64 @@
+/* tslint:disable:max-line-length */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'inicio',
+        title: 'Inicio',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/dashboard'
+    },
+    {
+        id   : 'tester',
+        title: 'Consulta Rapida',
+        type : 'basic',
+        icon : 'heroicons_outline:search',
+        link : '/quick-search'
+    },
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'inicio',
+        title: 'Inicio',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/dashboard'
+    },{
+        id   : 'tester',
+        title: 'Consulta Rapida',
+        type : 'basic',
+        icon : 'heroicons_outline:search',
+        link : '/quick-search'
+    },
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'inicio',
+        title: 'Inicio',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/dashboard'
+    },{
+        id   : 'tester',
+        title: 'Consulta Rapida',
+        type : 'basic',
+        icon : 'heroicons_outline:search',
+        link : '/quick-search'
+    },
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'inicio',
+        title: 'Inicio',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/dashboard'
+    },{
+        id   : 'tester',
+        title: 'Consulta Rapida',
+        type : 'basic',
+        icon : 'heroicons_outline:search',
+        link : '/quick-search'
+    },
+];
